ba77d5c72084a0a47d15ba17ba0bb77c46bf23bf
dishantzaveri <zaveridishant@gmail.com>
2024-07-03 02:26:14 +0530

    cgpa

--- src/portfolio.js
+++ src/portfolio.js
@@ -154,7 +154,7 @@ const educationInfo = {
       logo: require("./assets/images/djscamvi.png"),
       subHeader: "Bachelor of Technology in Computer Engineering",
       duration: "Dec 2020 - Jun 2024",
-      desc: "CGPA - 9.462.",
+      desc: "CGPA - 9.5.",
       descBullets: [
         "Honours in Intelligent Computing.",
         "Participated and won many national and international hackathons.",
